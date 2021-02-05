59a52de7fc6959bf6198dca11869ad4f6a07fc73
nidia <nidiacmh@gmail.com>
2021-02-05 15:47:06 -0600

    Modelo para manejar las paginas

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {HeroeModel} from '../../models/heroe.model';
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
+  heroe: HeroeModel = new HeroeModel();
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(form: NgForm){
+    if(form.invalid){
+      console.log('Formulario no valido');
+      return;
+    }
+    console.log(form);
+    console.log(this.heroe);
+
   }
 
 }
